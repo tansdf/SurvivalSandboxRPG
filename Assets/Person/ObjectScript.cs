1f387ebc7deee6ab32bba1a6f36062ec9b14709d
Zoider67 <iw.drozdoff2014@yandex.ru>
2018-12-07 11:17:49 +0300

    Начало описания взаимодействия персонажа с добываемыми обьектами
    - Дописан скрипт PlayerController. Чтобы добывать объекты нужно жать пробел
    - Добавлен новый скрипт ObjectScript(мб стоит переименовать). Этот скрипт должен висеть на префабах добываемых объектах, там обьявлены переменные здоровья, типа обьекта, методы при добыче.
    - Новый тег MinedObject для обьектов которые можно добывать

---
+++ Assets/Person/ObjectScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScript : MonoBehaviour {
+
+	public int hp;
+	public string TypeOfObject;
+
+	public void Hit()
+	{
+		hp--;
+		if (hp <= 0)
+			Destroy (gameObject);
+	}
+}
