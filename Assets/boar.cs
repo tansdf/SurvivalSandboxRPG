458033328dc8b51e7379e31a1f20f1a10dbff930
Konsyl <615153@mail.ru>
2018-12-15 22:59:24 +0300

    boar added alpha

---
+++ Assets/boar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boar : MonoBehaviour {
+
+    public float speed;
+    public float hp;
+    private float bx;
+    bool flagpos = true;
+    private float by;
+    float tm;
+    // Use this for initialization
+    void Start () {
+        float bx = transform.position.x;
+        float by = transform.position.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float x = transform.position.x;
+        float y = transform.position.y;
+        if (flagpos&(Mathf.Abs(bx - x) < 3 | Mathf.Abs(by - y) < 3)) { transform.Translate(Vector2.right * speed * Time.deltaTime); }
+        else {
+            if (flagpos) { tm = Time.realtimeSinceStartup; }
+
+
+            float tm2 = Time.realtimeSinceStartup;
+           
+           
+            if (Mathf.Abs(tm2-tm)> 7) { transform.Translate(Vector2.left * speed * Time.deltaTime); }
+           
+
+            flagpos = false;
+            if (Mathf.Abs(bx - x) < 0.5| Mathf.Abs(by - y) < 0.5) { flagpos = true; }
+        }
+       
+	}
+}
