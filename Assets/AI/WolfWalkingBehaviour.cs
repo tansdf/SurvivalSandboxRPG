7f1f2fc509af06a30950323efbfb9fed0da3cb10
ferrison <ne4ehbka98@gmail.com>
2019-07-11 04:06:00 +0300

    AI walking in wall on maximize bugfix

--- Assets/AI/WolfWalkingBehaviour.cs
+++ Assets/AI/WolfWalkingBehaviour.cs
@@ -7,20 +7,34 @@ public class WolfWalkingBehaviour : StateMachineBehaviour
     Vector2 prevPos;
     public float speed;
     Vector2 destination;
+    int counter;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       destination = Random.insideUnitCircle * 5;
+        prevPos = animator.transform.position;
+        destination = Random.insideUnitCircle * 5;
+        counter = 0;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if(Vector2.Distance(animator.transform.position, destination) < 0.1 || Vector2.Distance(prevPos, animator.transform.position) < 0.005)
+        Debug.Log(Time.deltaTime);
+        if(++counter == 5)
+        {
+            counter = 0;
+            prevPos = animator.transform.position;
+        }
+
+        if(counter == 4)
+        {
+            Debug.Log(Vector2.Distance(prevPos, animator.transform.position));
+        }
+
+        if(Vector2.Distance(animator.transform.position, destination) < 0.1 || (Vector2.Distance(prevPos, animator.transform.position) < 0.003 * speed && counter == 4))
         {
             animator.SetBool("isWalking", false);
         }
-        prevPos = animator.transform.position;
         animator.transform.position = Vector2.MoveTowards(animator.transform.position, destination, speed * Time.deltaTime);
         animator.SetFloat("Horizontal", (destination-(Vector2)animator.transform.position).normalized.x);
         animator.SetFloat("Vertical", (destination-(Vector2)animator.transform.position).normalized.y);
